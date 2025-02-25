7a21af506cd2b96f358d80da83611e925f8232ab
Kaizer-8 <165154339+Kaizer-8@users.noreply.github.com>
2025-02-25 14:11:51 +0100

    documentatie

--- Assets/SpeedBuff.cs
+++ Assets/SpeedBuff.cs
@@ -5,6 +5,6 @@ public class SpeedBuff : PowerupEffects
     public float Speed;
     public override void Apply(GameObject target)
     {
-        target.GetComponent<Movement>();
+        target.GetComponent<Movement>().speed.value += amount;//de speed van mijn movement class moet veranderd worden
     }
 }
