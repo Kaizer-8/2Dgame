503683605271c44261adce6e6fef5e6731318b31
Kaizer-8 <165154339+Kaizer-8@users.noreply.github.com>
2025-03-26 11:17:26 +0100

    mario platform

--- Assets/Scripts/CamerScript.cs
+++ Assets/Scripts/CamerScript.cs
@@ -5,7 +5,7 @@ public class CamerScript : MonoBehaviour
 {
     [SerializeField] private Vector3 offset;
     [SerializeField] private float damping;
-
+    //maakt een veld in unit om de waarden te veranderen
     public Transform target;
 
     private Vector3 vel = Vector3.one;
@@ -16,6 +16,7 @@ public class CamerScript : MonoBehaviour
         targetposition.z = transform.position.z;
 
         transform.position = Vector3.SmoothDamp(transform.position, targetposition, ref vel,damping);
+        //de positie van het gameobject staat gelijk aan de vector 3 wat de 3 waarden zijn om positie te vertellen x,y,z.
     }
 
 }
